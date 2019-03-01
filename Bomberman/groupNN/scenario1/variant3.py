9c0ae992e309b783b27557bc85050aaa6a0ba8e5
Unknown <ehschutzman@wpi.edu>
2019-03-01 12:16:08 -0500

    SC1 V3 90% win rate

--- Bomberman/groupNN/scenario1/variant3.py
+++ Bomberman/groupNN/scenario1/variant3.py
@@ -13,29 +13,49 @@ sys.path.insert(1, '../groupNN')
 from testcharacter import TestCharacter
 from qlearning_character import QCharacter
 
-for i in range(0, 10):
+# for i in range(0, 10):
     # Create the game
-    random.seed(i) # TODO Change this if you want different random choices
-    g = Game.fromfile('map.txt')
-    g.add_monster(SelfPreservingMonster("selfpreserving", # name
-                                        "S",              # avatar
-                                        3, 9,             # position
-                                        1                 # detection range
-                                        ))
+random.seed() # TODO Change this if you want different random choices
+g = Game.fromfile('map.txt')
+g.add_monster(SelfPreservingMonster("selfpreserving", # name
+                                    "S",              # avatar
+                                    3, 9,             # position
+                                    1                 # detection range
+                                    ))
 
-    # TODO Add your character
-    q = QCharacter(-100,  # wm
-                   100,  # wg
-                   -10,  # ww
-                   # weights[3],  # ww
-                   # weights[4],  # wcm
-                   # weights[5],  # wcg
-                   "Qlearn",  # name
-                   "Q",  # avatar
-                   0, 0  # position
-                   )
+# TODO Add your character
+q = QCharacter(-100,  # wm
+               100,  # wg
+               -10,  # ww
+               # weights[3],  # ww
+               # weights[4],  # wcm
+               # weights[5],  # wcg
+               "Qlearn",  # name
+               "Q",  # avatar
+               0, 0  # position
+               )
 
-    g.add_character(q)
+g.add_character(q)
 
-    # Run!
-    g.go()
+# Run!
+g.go()
+
+#
+# win = 0
+# loss = 0
+#
+# with open('results.txt', 'r') as infile:
+#     lines = infile.readlines()
+#     for lin in lines:
+#         if 'win' in lin:
+#             win+= 1
+#         else:
+#             loss+= 1
+#
+# print("GAMES::", (win+loss))
+# print("WIN:::", win/(win+loss))
+# print("LOSS::", loss/(win+loss))
+#
+# with open('results.txt', 'w') as r:
+#     r.write("")
+
